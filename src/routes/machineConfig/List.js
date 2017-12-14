e6e910646e6e4f02b5a4ab89ed9917bbebb7d33f
chenkechao <chenkechao@zto.cn>
2017-12-14 15:41:56 +0800

    machine功能

--- src/routes/machineConfig/List.js
+++ src/routes/machineConfig/List.js
@@ -12,16 +12,67 @@ const confirm = Modal.confirm
 const List = ({ onDeleteItem, onEditItem, onRuleConfig, onSelectServer, onJobStatus, isMotion, location, ...tableProps }) => {
   location.query = queryString.parse(location.search)
 
+  let spanSum = 0
+
   const columns = [
     {
       title: 'ip',
       dataIndex: 'machine.ip',
       key: 'ip',
+      render: (value, row, index) => {
+        const obj = {
+          children: value,
+          props: {},
+        };
+
+        // if(spanSum > 0){
+        //   if(index < spanSum-1){
+        //     obj.props.rowSpan = 0
+        //   }else if(index == spanSum-1){
+        //     spanSum += row.machine.jobSize
+        //     obj.props.rowSpan = row.machine.jobSize
+        //   }
+        // }else{
+        //   if(row.machine.jobSize > 0){
+        //     spanSum = row.machine.jobSize
+        //     if(spanSum > 1){
+        //       obj.props.rowSpan = 0
+        //     }
+        //   }
+        // }
+        if(row.machine.jobSize > 1){
+          if(index == spanSum || spanSum == 0){
+            spanSum += row.machine.jobSize
+            obj.props.rowSpan = row.machine.jobSize
+          }else{
+            obj.props.rowSpan = 0
+          }
+        }else{
+          spanSum ++
+        }
+
+        return obj;
+      },
     },
     {
       title: 'job',
       dataIndex: 'jobId',
       key: 'job',
+      // render: (text, record, index) => {
+      //   // const obj = {
+      //   //   children: value,
+      //   //   props: {},
+      //   // }
+      //   // if (index === 4) {
+      //   //   obj.props.colSpan = 0
+      //   // }
+      //   // return obj
+      //   if (record.hasOwnProperty('jobOperates')&&record.jobOperates != null) {
+      //     return record.jobOperates.map((item) => {
+      //       return <Tag color="blue">{item.jobId}</Tag>
+      //     })
+      //   }
+      // },
     },
   ]
 
